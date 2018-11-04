e8588d2ad7e0311809555f62d326a2157ba76828
Nicholas Pufal <npufal@doximity.com>
2018-11-04 14:12:32 -0200

    Refactor: use method_missing to avoid redundancy
    This has downsides, as noted in the code.
    But it's fun and this is a fun/hacky project anyway :)

--- app/config_reader.rb
+++ app/config_reader.rb
@@ -9,49 +9,23 @@ class ConfigReader
     @yaml = YAML.load_file("config.yml")
   end
 
-  def extra_info
-    data.extra_info
-  end
-
-  def from
-    data.from
-  end
-
-  def to
-    data.to
-  end
-
-  def date_format
-    settings.date_format
-  end
-
-  def currency_format
-    settings.currency_format
-  end
-
-  def rate
-    settings.rate
-  end
-
-  def due_day
-    settings.due_day
-  end
-
-  def invoice_identifier
-    settings.invoice_identifier
-  end
-
-  def filename
-    settings.filename
-  end
-
-  private
-
   def settings
     @settings ||= OpenStruct.new(@yaml["settings"])
   end
 
   def data
     @data ||= OpenStruct.new(@yaml["data"])
+  end
+
+  # Hacky way to avoid having to define a method for each of the config properties.
+  # Here just for fun and to make it easier to play around with the code.
+  # This causes undesireable downstream effects, i.e. calling `respond_to?(:anything)`
+  # would return true, which it's fine in this codebase/project
+  def method_missing(method, *args, &block)
+    settings.public_send(method) || data.public_send(method) || super
+  end
+
+  def respond_to_missing?(method_name, *args)
+    true
   end
 end
